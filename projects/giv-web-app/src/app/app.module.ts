96611a5f3ac7bc44dd80d8a645162e09493b4f83
Touré Holder <toureholder@gmail.com>
2020-07-18 18:43:52 -0300

    install swiper

--- projects/giv-web-app/src/app/app.module.ts
+++ projects/giv-web-app/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    CoreModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects/giv-web-app/src/app/shared/shared.module.ts
+++ projects/giv-web-app/src/app/shared/shared.module.ts
@@ -3,9 +3,29 @@ import { CommonModule } from '@angular/common';
 import { FeaturedListingImageComponent } from './components/featured-listing-image/featured-listing-image.component';
 import { RouterModule } from '@angular/router';
 
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+};
+
 @NgModule({
   declarations: [FeaturedListingImageComponent],
-  imports: [RouterModule, CommonModule],
-  exports: [RouterModule, CommonModule, FeaturedListingImageComponent],
+  imports: [RouterModule, CommonModule, SwiperModule],
+  exports: [
+    RouterModule,
+    CommonModule,
+    FeaturedListingImageComponent,
+    SwiperModule,
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG,
+    },
+  ],
 })
 export class SharedModule {}
